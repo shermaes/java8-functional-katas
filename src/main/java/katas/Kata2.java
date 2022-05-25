66bbf7be6057e9e1ee0cf378764dbe8dea26c55a
Sherilyn Maestre <you@example.com>
2022-05-25 15:34:00 -0500

    solved kata3

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -19,12 +19,16 @@ public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-    List<Integer> videos = movies.stream()
+        List<Integer> videos = movies.stream()
         .filter(i -> i.getRating()==5)
         .map(m->m.getId())
             .collect(Collectors.toList());
         System.out.println(videos);
-
         return videos;
     }
+    //1) I create a list of Integer objects
+    //2) with movies.stream() I create a stream of movies objects
+    //3) I use filter to get the objects with rating 5 using the condition passed on the lambda
+    //4)  I use .map to return a stream consisting of the results of applying the given function
+    //5)with collect(Collectors.toList()) I accumulate the results into a list
 }
--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -6,6 +6,7 @@ import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -16,6 +17,21 @@ public class Kata3 {
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        List<Integer> videoIds = movieLists.stream()
+                .flatMap(videos -> videos.getVideos().stream())
+                .map(id -> id.getId().intValue())
+                .collect(Collectors.toList());
+        System.out.println(videoIds);
+
+        return videoIds;
+
+        //1)I create a list of Integers
+        //2) with movieLists.stream() I create a stream of movies objects
+        //3) what does flatMap do?
+        //Returns a stream consisting of the results of replacing each element of the stream I created in 1)
+        // with the contents of a mapped stream produced by applying the provided mapping function to each element.
+        //4) I get the int value of the id using .map()
+        //5) with collect(Collectors.toList()) I accumulate the results into a list
     }
+
 }
